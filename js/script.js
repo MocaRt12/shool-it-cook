e5325a414ee0ed9e937e26d003307756c003885b
MocaRt12 <108404816+MocaRt12@users.noreply.github.com>
2022-06-30 13:53:57 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let colorEyes = 'green';	
+let user = '';
+let userName = 'Вася';
+
+user = userName;
+console.log(user);
+
+
+
+
+
+let userAge = 58;
+console.log(typeof userAge);
+console.log(userAge);
+
+userAge = String(userAge);
+console.log(`Я родился уже как ${userAge} лет назад`);
