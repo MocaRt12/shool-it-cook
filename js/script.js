b041545453b8794ddfc8ef6abe6214217ffe20d9
MocaRt12 <nikitadubovik612@gmail.com>
2022-07-01 22:15:47 +0300

    added operation

--- js/script.js
+++ js/script.js
@@ -21,3 +21,14 @@ console.log(`Я родился уже как ${userAge} лет назад`);
 
 
 console.log('Test 2');
+
+
+
+
+
+console.log('35' + - "22");
+console.log( '558' > "++22");
+console.log(!false && 11 || 18 && !'');
+
+let name = 0;
+console.log(name ?? "без имени");
