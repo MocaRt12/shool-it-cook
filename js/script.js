601747f1356da98c7fd0bcb2a8cbbaf747cf3b82
MocaRt12 <nikitadubovik612@gmail.com>
2022-07-05 22:47:10 +0300

    added if and else

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/*
 let colorEyes = 'green';
 let user = '';
 let userName = 'Вася';
@@ -32,3 +33,47 @@ console.log(!false && 11 || 18 && !'');
 
 let name = 0;
 console.log(name ?? "без имени");
+
+
+
+
+
+let message = "Я фрилансер";
+let first = 3;
+let second = 8;
+
+if(first === second) {
+	console.log(message); 
+} else {
+	console.log("Ответ не найден");
+}
+
+let message2 = (21 > 16 || 3 < 8) ? 'Истина' : 'ложь';
+
+if(first === second) {
+	console.log('number one');
+} else  if(1 > 2){
+	console.log('number two');
+} else{
+	console.log(':(')
+}
+*/
+
+let numberOne = 1;
+while(numberOne < 5){
+	console.log(numberOne);
+	numberOne += 1;
+}
+
+let num = 0;
+do{
+	console.log(num);
+	num++;
+} while(num < 4);
+
+/* ОПАСНО!!!!
+for(let numberTwo = 1; numberTwo < 5; numberTwo++){
+	console.log(--numberTwo);
+}
+*/
+
